324f0ccb35398678ddb2c032d5409164c0d05f90
Vasily Melenchuk <bzzzil@gmail.com>
2017-10-31 14:06:28 +0000

    chore: disable debug stuff

--- build.py
+++ build.py
@@ -7,11 +7,11 @@ if __name__ == "__main__":
     builder = ConanMultiPackager(args="--build missing")
     builder.add_common_builds(shared_option_name="libjpeg:shared", pure_c=True)
 
-    new_builds = []
-    for settings, options, env_vars, build_requires in builder.builds:
-        if settings["arch"] == "x86" or settings["compiler.version"] != "5.4":
-            continue
-        new_builds.append((settings, options, env_vars, build_requires))
+#    new_builds = []
+#    for settings, options, env_vars, build_requires in builder.builds:
+#        if settings["arch"] == "x86" or settings["compiler.version"] != "5.4":
+#            continue
+#        new_builds.append((settings, options, env_vars, build_requires))
 
     builder.builds = new_builds
 
